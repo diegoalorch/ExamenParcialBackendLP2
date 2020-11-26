2a6071961335f4d9646b618eefae14b054910095
Diego Alor <diegoalor@upeu.edu.pe>
2020-11-26 18:22:59 -0500

    version 2.0

--- src/main/java/pe/apirestoracle/controller/ProductoController.java
+++ src/main/java/pe/apirestoracle/controller/ProductoController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,5 +38,9 @@ public class ProductoController {
 		producto.setIdproducto(id);;
 		
 	return productoService.update(producto);
+	}
+	@DeleteMapping("/delete/{id}")
+	public int delete(@PathVariable int id ) {
+		return productoService.delete(id);
 	}
 }
